d7ace09a9ea1f1ebe41882cccac2bfca653dcbb3
Natalie Carvalho <73089379+natscarv@users.noreply.github.com>
2021-11-27 01:51:54 -0300

    Add files via upload

---
+++ Drag-name.py
@@ -0,0 +1,10 @@
+#A greeting to the user
+print("Welcome to Drag Queen name generator")
+
+print("With this app, we will find out what's your Drag name")
+#Inputs for the first and second name 
+firstname = input("Which city did you grow up? ")
+
+secondname = input("What was the last food that you eat? ")
+#result
+print("Based on your answers, you can call yourself " + firstname + " " + secondname, "congratulations!")
